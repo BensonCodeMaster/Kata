353623825214accc4adad6f30f5fcd163a42ce2e
Denis Babushkin <bensoncodemaster@gmail.com>
2025-03-28 18:07:34 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        // реализуйте алгоритм
+        UserService userService = new UserServiceImpl(new UserDaoJDBCImpl());
+
+        userService.createUsersTable();
+        userService.saveUser("Ivan","Ivanov",(byte) 20);
+        userService.saveUser("Misha","Petrov",(byte) 20);
+        userService.saveUser("Oleg","Sidorov",(byte) 20);
+        userService.saveUser("Vladislav","Horstmann",(byte) 20);
+
+        for (User u : userService.getAllUsers()) {
+            System.out.println(u);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Den4ik13021999";
+
+    public static Connection getConnection() throws SQLException {
+       return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
 }
